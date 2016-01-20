9ee33183779de47c47bf2210f4df43d6a71b48d0
jaeseok an <k1462487@kcl.ac.uk>
2016-01-20 18:37:30 +0000

    first commit

---
+++ OOP1.java
@@ -0,0 +1,25 @@
+package OOP;
+
+public class OOP1 {
+
+
+	 static int tenToTwo (int n){
+		 if(n>0){
+			 int tmp;
+		    tmp = n%2;
+		  n/=2;
+		 tenToTwo(n);
+		 System.out.print(tmp);}
+		  
+		return 0;
+		 
+	}
+	 
+	
+	
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+	tenToTwo(5);
+	}
+
+}
