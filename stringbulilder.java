416d45d2c12c9c4fe7b1f503c90428d7b32d890c
jaeseok an <k1462487@kcl.ac.uk>
2016-01-21 21:48:55 +0000

    string builder practice

---
+++ stringbulilder.java
@@ -0,0 +1,26 @@
+package OOP;
+
+public class stringbulilder {
+
+	
+	public static void main(String[] args) {
+		String str = "940520-1234563";
+
+		StringBuilder strb = new StringBuilder(str);
+		
+		for(int i=0;i<strb.length();i++){
+			 
+			if(strb.charAt(i)=='-'){
+				strb.deleteCharAt(i);
+				break;
+			} else{
+				
+			}
+		}
+		str=strb.toString();
+		System.out.println(str);
+
+
+	}
+
+}
