ad677b570ad92684631dd597ec49b997422e2a62
jaeseok an <k1462487@kcl.ac.uk>
2016-01-21 18:23:59 +0000

    static method

---
+++ staticpractice.java
@@ -0,0 +1,37 @@
+package OOP;
+
+
+class SimpleNumber{
+	int num = 0 ;
+	private SimpleNumber(){}
+	public void addNumb(int n){num+=n;}
+	public void showNumb(){System.out.println(num);}
+	
+	private static SimpleNumber inst = null;
+	public static SimpleNumber getSimpleNumberInst(){
+		
+		if(inst==null)
+			inst = new SimpleNumber();
+		
+		
+		return inst;
+	}
+}
+
+
+public class staticpractice {
+
+	public static void main(String[] args) {
+		
+		SimpleNumber num1 = SimpleNumber.getSimpleNumberInst();
+		num1.addNumb(20);
+
+		SimpleNumber num2 = SimpleNumber.getSimpleNumberInst();
+		num2.addNumb(30);
+		
+		num1.showNumb();
+		num2.showNumb();
+
+	}
+
+}
