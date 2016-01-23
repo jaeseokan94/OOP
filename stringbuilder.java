483c1959d930ca078bc1a377735dd72f7b411423
jaeseok an <k1462487@kcl.ac.uk>
2016-01-23 23:50:44 +0000

    scanner

--- stringbuilder.java
+++ stringbuilder.java
@@ -1,5 +1,7 @@
 package OOP;
 
+import java.util.Scanner;
+
 public class stringbuilder {
 
 	
@@ -19,8 +21,21 @@ public class stringbuilder {
 		}
 		str=strb.toString();
 		System.out.println(str);
+		
+		
+		
+		System.out.printf("\"hello\" \n hi %d " ,50 );
 
+		String source = "1 5 7 ";
+		Scanner sc = new Scanner(source);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int sum = a+b+c;
+
+		System.out.printf("\n%d+%d+%d=%d",a,b,c,sum);
 
 	}
 
 }
+//cd documents/workspace/projectsiliconvalley/src/oop
